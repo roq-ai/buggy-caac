a8af83c2ed70ed7f54b3baafb01b4921e873594f
roq-ai <>
2023-07-02 17:35:27 +0000

    Initial commit

---
+++ src/interfaces/transaction/index.ts
@@ -0,0 +1,22 @@
+import { OrganizationInterface } from 'interfaces/organization';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface TransactionInterface {
+  id?: string;
+  amount: number;
+  organization_id?: string;
+  user_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  organization?: OrganizationInterface;
+  user?: UserInterface;
+  _count?: {};
+}
+
+export interface TransactionGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  organization_id?: string;
+  user_id?: string;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,102 @@
+import { Button, Flex, Heading, Image, Text, Stack, useBreakpointValue, Box, Link } from '@chakra-ui/react';
+
+import { signIn, signUp, requireNextAuth } from '@roq/nextjs';
+
+import Head from 'next/head';
+import { HelpBox } from 'components/help-box';
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>{`Buggy`}</title>
+
+        <meta
+          name="description"
+          content="Experience the future of digital transactions with Buggy, your reliable and secure Crypto wallet web 3.0. Your gateway to seamless cryptocurrency management."
+        />
+      </Head>
+
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack position="relative" spacing={6} w={'full'} maxW={'lg'}>
+            <HelpBox />
+            <Image src="/roq.svg" alt="Logo" w="150px" mb="8" />
+            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+              <Text as={'span'}>Explore</Text>{' '}
+              <Text
+                as={'span'}
+                position={'relative'}
+                _after={{
+                  content: "''",
+                  width: 'full',
+                  height: useBreakpointValue({ base: '20%', md: '30%' }),
+                  position: 'absolute',
+                  bottom: 1,
+                  left: 0,
+                  bg: 'cyan.400',
+                  zIndex: -1,
+                }}
+              >
+                {`Buggy`}
+              </Text>
+            </Heading>
+            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+              {`Experience the future of digital transactions with Buggy, your reliable and secure Crypto wallet web 3.0. Your gateway to seamless cryptocurrency management.`}
+            </Text>
+            <Stack direction="column" spacing={4} className="roles-container">
+              <Stack className="owner-roles-container" direction={{ base: 'column', md: 'row' }} spacing={4}>
+                <Button
+                  rounded={'full'}
+                  bg={'cyan.500'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'cyan.700',
+                  }}
+                  onClick={() => signUp('owner')}
+                >
+                  Create Account
+                </Button>
+                <Button rounded={'full'} onClick={() => signIn('owner')}>
+                  Login
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex position="relative" flex={1}>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1666635376217-ce550caea63f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjA3NjB8MHwxfHNlYXJjaHwxfHxjcnlwdG8lMjB3YWxsZXQlMkN3ZWIlMjAzLjB8ZW58MHx8fHwxNjg4MzE5MjgwfDA&ixlib=rb-4.0.3&q=80&w=1080'
+            }
+            alt={'Login Image'}
+            objectFit={'cover'}
+          />
+          <Box position="absolute" top="0" backgroundColor="rgba(0,0,0,0.6)" width="100%" py="2">
+            <Text align="center" fontSize="sm" color="white">
+              Photo by{' '}
+              <Link
+                href="https://unsplash.com/@shubzweb3?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >{`Shubham's Web3`}</Link>{' '}
+              on{' '}
+              <Link
+                href="https://unsplash.com/?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >
+                Unsplash
+              </Link>
+            </Text>
+          </Box>
+        </Flex>
+      </Stack>
+    </>
+  );
+}
+
+export default requireNextAuth({
+  redirectIfAuthenticated: true,
+  redirectTo: '/users',
+})(HomePage);
